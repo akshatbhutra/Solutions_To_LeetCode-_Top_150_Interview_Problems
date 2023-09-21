f2c07de186cb06120975034760af968946b6444d
akshat - bhutra <akshat_bt18@iiitkalyani.ac.in>
2023-09-21 18:19:46 +0530

    add lps solution

---
+++ LongestPalindromicSubstring.cpp
@@ -0,0 +1,34 @@
+// Approach:
+// define following:
+// dp[i][j] = 1 if s[i...j] is a palindrome, else 0
+// Main observation is s[i...j] is a palindrome iff s[i]=s[j] && s[i+1...j-1] is a palindrome
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        // start = starting position of resultant answer string & len is its length
+        int n = s.size(), start, len = 1;
+        bool dp[n][n];
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++) 
+                dp[i][j] = 0;
+        for (int i = 0; i < n; i++)
+            dp[i][i] = 1;
+        for (int i = 0; i < n - 1; i++) 
+            if (s[i] == s[i + 1]) {
+                dp[i][i + 1] = 1;
+                len = 2;
+                start = i;
+            }
+        for (int k = 3; k <= n; k++)
+            for (int i = 0; i < n - k + 1; i++) {
+                int j = i + k - 1;
+                if (dp[i + 1][j - 1] && s[i] == s[j]) {
+                    dp[i][j] = 1;
+                    len = k;
+                    start = i;
+                }
+            }
+        return s.substr(start, len);
+    }
+};
