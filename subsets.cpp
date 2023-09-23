abbcde2c653c7bf40c398c10f503da61552d3091
akshat - bhutra <akshat_bt18@iiitkalyani.ac.in>
2023-09-23 10:13:05 +0530

    add solution to subsets

---
+++ subsets.cpp
@@ -0,0 +1,19 @@
+// Approach:
+// Iterate over all 2^n subsets
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n = (int)(nums.size());
+        for (int i = 0; i < (1<<n); i++) {
+            vector<int> t;
+            for (int j = 0; j < n; j++) {
+                if (i & (1 << j))
+                    t.push_back(nums[j]);
+            }
+            ans.push_back(t);
+        }
+        return ans;
+    }
+};
