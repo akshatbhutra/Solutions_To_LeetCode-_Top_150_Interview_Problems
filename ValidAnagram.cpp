43fbb40e83ef150295763c10e7dac255259ce778
akshat - bhutra <akshat_bt18@iiitkalyani.ac.in>
2023-09-24 13:04:56 +0530

    add solution to valid anagram

---
+++ ValidAnagram.cpp
@@ -0,0 +1,12 @@
+//
+// approach:
+// sort both strings and check equality
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return (s == t);
+    }
+};
