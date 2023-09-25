39971e117b208c398f4aedbcfc9539d1a7b60939
akshat - bhutra <akshat_bt18@iiitkalyani.ac.in>
2023-09-25 12:54:42 +0530

    optimize solution

--- ReverseBits.cpp
+++ ReverseBits.cpp
@@ -1,33 +1,16 @@
 // https://leetcode.com/problems/reverse-bits/
 // Approach:
+// ith bit will become 31 - i th bit in reverse number
 // Iterate over bits and sum their contribution
 
 class Solution {
-public:
-    string to_binary(uint32_t n) {
-            string ans;
-            while (n > 0) {
-                if (n & 1)
-                    ans = ans + '1';
-                else
-                    ans = ans + '0';
-                n >>= 1;
-            }
-        while((int)ans.size() != 32) 
-            ans = ans + '0';
-        return ans;
-    }
-    
-    uint32_t to_deci(string b) {
-        uint32_t ans = 0;
-        reverse(b.begin(), b.end());
-        for (int i = 0; i < (int)(b.size()); i++)
-            if (b[i] == '1')
-                ans += (1 << i);
-        return ans;
-    }
-    
+public:  
     uint32_t reverseBits(uint32_t n) {
-        return to_deci(to_binary(n));
+        uint32_t ans = 0;
+        for (int i = 0; i <= 31; i++) {
+            if (n & (1<<i))
+                ans += (1 << (31 - i));
+        }
+        return ans;
     }
 };
