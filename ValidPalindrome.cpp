dba0de30e3d005797b011a0d2e9e2315df75cc5d
akshat - bhutra <akshat_bt18@iiitkalyani.ac.in>
2023-09-25 11:19:29 +0530

    add another solution to valid palindrome

--- ValidPalindrome.cpp
+++ ValidPalindrome.cpp
@@ -1,18 +1,41 @@
 // Approach:
 // Simple Implementation
 
+// class Solution {
+// public:
+//     bool isPalindrome(string s) {
+//         int n = (int)(s.size());
+//         string t;
+//         for (int i = 0; i < n; i++)
+//             if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= '0' && s[i] <= '9'))
+//                 t += s[i];
+//         int m = (int)t.size();
+//         for (int i = 0; i < m; i++)
+//             if (t[i] >= 'A' && t[i] <= 'Z')
+//                 t[i] = (char)(t[i] + 32);
+//         bool ok = 1;
+//         for (int i = 0; i < m / 2; i++)
+//             if (t[i] != t[m - 1 - i])
+//                 ok = 0;
+//         return (ok == 0 ? false : true);
+//     }
+// };
+
+// Approach:
+// use isalnum & tolower
+
 class Solution {
 public:
     bool isPalindrome(string s) {
         int n = (int)(s.size());
         string t;
         for (int i = 0; i < n; i++)
-            if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= '0' && s[i] <= '9'))
+            if (isalnum(s[i]))
                 t += s[i];
         int m = (int)t.size();
         for (int i = 0; i < m; i++)
             if (t[i] >= 'A' && t[i] <= 'Z')
-                t[i] = (char)(t[i] + 32);
+                t[i] = tolower(t[i] + 32);
         bool ok = 1;
         for (int i = 0; i < m / 2; i++)
             if (t[i] != t[m - 1 - i])
