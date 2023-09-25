94862f6837791d99422ed11ee1224bdc0e2eced8
akshat - bhutra <akshat_bt18@iiitkalyani.ac.in>
2023-09-25 17:09:29 +0530

    optimized snowball approach

--- MoveZeroes.cpp
+++ MoveZeroes.cpp
@@ -1,24 +1,18 @@
 // https://leetcode.com/problems/move-zeroes/
 // approach:
-// iterate and whenever encounter a zero swap with next non zero value
+// https://leetcode.com/problems/move-zeroes/discuss/172432/THE-EASIEST-but-UNUSUAL-snowball-JAVA-solution-BEATS-100-(O(n))-%2B-clear-explanation
+// snowball approach
 
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
-        int n = (int)(nums.size());
-        set<int> s;
-        for (int i = 0; i < n; i++)
-            if(nums[i] != 0)
-                s.insert(i);
-        for (int i = 0; i < n; i++) {
-            if(nums[i] == 0 && !s.empty()) {
-                int non_zero_index;
-                auto it = s.upper_bound(i);
-                if (it == s.end())
-                    break;
-                non_zero_index = *it;
-                s.erase(non_zero_index);
-                swap(nums[i], nums[non_zero_index]);
+    void moveZeroes(vector<int> &nums) {
+        int snowBallSize = 0; 
+        for (int i=0;i<nums.size();i++){
+	        if (nums[i]==0){
+                snowBallSize++; 
+            }
+            else if (snowBallSize > 0) {
+	            swap(nums[i], nums[i - snowBallSize]);
             }
         }
     }
