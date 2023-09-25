57b4489335b6e913eecb29758d26031bdb0ad69d
akshat - bhutra <akshat_bt18@iiitkalyani.ac.in>
2023-09-25 16:05:04 +0530

    clean code

--- PowerOfThree.cpp
+++ PowerOfThree.cpp
@@ -1,6 +1,43 @@
 // https://leetcode.com/problems/power-of-three/
-// approach
+// approach1
 // atmost log3(n) iterations 
+
+// class Solution {
+// public:
+//     bool isPowerOfThree(int n) {
+//         if (n == 1)
+//             return true;
+//         if (n <= 2)
+//             return false;
+//         long long p = 1;
+//         int limit = 21; // log3(MAX)
+//         for (int i = 1; i <= limit; i++) {
+//             p *= 3;
+//             if (p == n) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+// Approach2:
+// Recursion
+
+// class Solution {
+// public:
+//     bool isPowerOfThree(int n) {
+//         if (n == 0)
+//             return false;
+//         if (n == 1)
+//             return true;
+//         if (n % 3 != 0)
+//             return false;
+//         return isPowerOfThree(n / 3);
+//     }
+// };
+
+// another implementation
 
 class Solution {
 public:
@@ -9,14 +46,14 @@ public:
             return true;
         if (n <= 2)
             return false;
-        long long p = 1;
-        int limit = 21; // log3(MAX)
-        for (int i = 1; i <= limit; i++) {
-            p *= 3;
-            if (p == n) {
-                return true;
-            }
+        while (n != 1) {
+            if (n % 3 != 0)
+                return false;
+            n /= 3;
         }
-        return false;
+        return true;
     }
 };
+
+// Follow up: Could you solve it without loops/recursion?
+// Calculate 3^19 by hand & check if n divides 3^19 as 3^20 overflows in int
