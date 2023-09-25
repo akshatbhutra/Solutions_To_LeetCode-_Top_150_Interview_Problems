30029b1f0ff530908d8c884ca02216ff5196e3cc
akshat - bhutra <akshat_bt18@iiitkalyani.ac.in>
2023-09-25 12:13:38 +0530

    optimize space complexity

--- IntersectionOfTwoLinkedLists.cpp
+++ IntersectionOfTwoLinkedLists.cpp
@@ -8,7 +8,15 @@
  */
 
 // approach:
-// generate two lists of pointers and see their intersection
+// O(1) space solution
+// After intersection both linked lists will have same size
+// before intersection their sizes may differ
+// find the difference "diff"
+// then move head pointer in larger list "diff" steps ahead
+// now both linked lists are of same size (counting from their head)\
+// now check for intersection
+
+
 class Solution {
 public:
     ListNode* make(vector<int> v) {
@@ -31,13 +39,43 @@ public:
         return v;
     }
     
+    
+    int getSize(ListNode* r) {
+        int ans = 0;
+        while(r != NULL) {
+            ans++;
+            r = r->next;
+        }
+        return ans;
+    }
+    
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        map<ListNode*, bool> vis;
-        for (auto &e: get(headA))
-            vis[e] = 1;
-        for (auto &e: get(headB))
-            if(vis[e])
-                return e;
+        int l1 = getSize(headA), l2 = getSize(headB);
+        ListNode* origheadA = headA, *origheadB = headB, *ans;
+        
+        int diff = abs(l1 - l2);
+        if (l1 > l2) {
+            for (int i = 0; i < diff; i++)
+                headA = headA->next;
+        }
+        else {
+            for (int i = 0; i < diff; i++)
+                headB = headB->next;
+        }
+        while(1) {
+            if (headA == NULL)
+                break;
+            if (headA == headB) {
+                ans = headA;
+                headA = origheadA;
+                headB = origheadB;
+                return ans;
+            }
+            headA = headA->next;
+            headB = headB->next;
+        }
+        headA = origheadA;
+        headB = origheadB;
         return NULL;
     }
 };
