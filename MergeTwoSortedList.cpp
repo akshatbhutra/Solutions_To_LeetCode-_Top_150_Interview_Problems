bbc8d624a3d5d5cf8ee0a133c3ce3ba740217c15
akshat - bhutra <akshat_bt18@iiitkalyani.ac.in>
2023-09-25 10:26:30 +0530

    add iterative solution

--- MergeTwoSortedList.cpp
+++ MergeTwoSortedList.cpp
@@ -11,6 +11,37 @@
 
 // Recursive Solution
 
+// class Solution {
+// public:
+//     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//         if (list1 == NULL)
+//             return list2;
+//         if (list2 == NULL)
+//             return list1;
+//         if (list1->val < list2->val)  {
+//             list1->next = mergeTwoLists(list1->next, list2);
+//             return list1;
+//         }
+//         else  {
+//             list2->next = mergeTwoLists(list2->next, list1);
+//             return list2;
+//         }
+//     }
+// };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+// Iterative Solution
+
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
@@ -18,13 +49,33 @@ public:
             return list2;
         if (list2 == NULL)
             return list1;
-        if (list1->val < list2->val)  {
-            list1->next = mergeTwoLists(list1->next, list2);
-            return list1;
+        ListNode* root;
+        if (list1->val < list2->val) {
+            root = list1;
+            list1 = list1->next;
         }
-        else  {
-            list2->next = mergeTwoLists(list2->next, list1);
-            return list2;
+        else {
+            root = list2;
+            list2 = list2->next;
         }
+        ListNode* cur = root;
+        while (list1 != NULL && list2 != NULL) {
+            if (list1->val < list2->val) {
+                cur->next = list1;
+                list1 = list1->next;
+            }
+            else {
+                cur->next = list2;
+                list2 = list2->next;
+            }
+            cur = cur -> next;
+        }
+        if (list1 == NULL) {
+            cur->next = list2; 
+        }
+        else if (list2 == NULL) {
+            cur->next = list1;
+        }
+        return root;
     }
 };
