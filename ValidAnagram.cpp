4504b75d2c86c40c8c307fb2559b06457cc11b33
akshat - bhutra <akshat_bt18@iiitkalyani.ac.in>
2023-09-25 15:45:05 +0530

    add optimized solution

--- ValidAnagram.cpp
+++ ValidAnagram.cpp
@@ -1,12 +1,35 @@
 //
-// approach:
+// approach1:
 // sort both strings and check equality
 
-class Solution {
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         sort(s.begin(), s.end());
+//         sort(t.begin(), t.end());
+//         return (s == t);
+//     }
+// };
+
+// approach2
+// use freq array of size 26
+
+class Solution {               
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return (s == t);
+        if(s.length() != t.length()){
+            return false;
+        }
+        int freq[26] = {0};
+        for(int i=0; i<s.length(); i++){
+            freq[s[i]-'a']++;
+            freq[t[i]-'a']--;
+        }
+        for(int i=0; i<26; i++){
+            if(freq[i] != 0){
+                return false;
+            }
+        }
+        return true;
     }
 };
