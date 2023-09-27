3037547697e60df4e93d000ba3405107c38b58c0
akshat - bhutra <akshat_bt18@iiitkalyani.ac.in>
2023-09-27 16:06:06 +0530

    optimize space

--- IncreasingTripletSubsequence.cpp
+++ IncreasingTripletSubsequence.cpp
@@ -1,44 +1,31 @@
-// https://leetcode.com/problems/increasing-triplet-subsequence/discuss/2688485/Two-Approaches-oror-Easy-To-Understand-oror-C%2B%2B-Code
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
 
         int n = nums.size();
         
-        // left_min[i] will store the minimum from left till ith
-
-        vector<int> left_min(n);
+        // first will keep track of first element of triplet
         
-        // right_max[i] will store the maximum from right till ith
-
-        vector<int> right_max(n);
+        int first = INT_MAX;
         
-        // fill left_min array
-
-        left_min[0] = nums[0];
-
-        for(int i = 1; i < n; i++)
+        // second will keep track of second element of triple
+        
+        int second = INT_MAX;
+        
+        // second > first
+        
+        for(int i = 0; i < n; i++)
         {
-            left_min[i] = min(left_min[i - 1], nums[i]);
-        }
-        
-        // fill right_max array
-
-        right_max[n - 1] = nums[n - 1];
-
-        for(int i = n - 2; i >= 0; i--)
-        {
-            right_max[i] = max(right_max[i + 1], nums[i]);
-        }
-        
-        // check that is there any element which has smaller element on left side and greater element on right side
-
-        for(int i = 1; i < n - 1; i++)
-        {
-            if(left_min[i - 1] < nums[i] && nums[i] < right_max[i + 1])
+            if(nums[i] <= first)
             {
-                return true;
+                first = nums[i];
             }
+            else if(nums[i] <= second)
+            {
+                second = nums[i];
+            }
+            else
+                return true;
         }
         
         return false;
