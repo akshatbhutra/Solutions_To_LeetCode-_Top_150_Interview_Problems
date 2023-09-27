7c57f30efcbbe41844e0b83b06829dcb1042e121
akshat - bhutra <akshat_bt18@iiitkalyani.ac.in>
2023-09-27 12:21:04 +0530

    optimize space

--- HouseRobber.cpp
+++ HouseRobber.cpp
@@ -2,18 +2,37 @@
 // approach:
 //  dp[i] = ans till initial i values
 
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//         int n = (int)(nums.size());
+//         if (n == 1)
+//             return nums[0];
+//         int dp[n];
+//         dp[0] = nums[0];
+//         dp[1] = max(nums[0], nums[1]);
+//         for (int i = 2; i < n; i++) {
+//             dp[i] = max(dp[i - 1], nums[i] + dp[i - 2]);
+//         }
+//         return dp[n - 1];
+//     }
+// };
+
+// O(1) space
+
 class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = (int)(nums.size());
         if (n == 1)
             return nums[0];
-        int dp[n];
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        // int dp[n];
+        int cur, prev = max(nums[0], nums[1]), prev_prev = nums[0];
         for (int i = 2; i < n; i++) {
-            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2]);
+            cur = max(prev, nums[i] + prev_prev);
+            prev_prev = prev;
+            prev = cur;
         }
-        return dp[n - 1];
+        return prev;
     }
 };
